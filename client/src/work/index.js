b4687a2d582972304018578cdb8064094224cc54
Christopher Lee <hanernlee@gmail.com>
2017-08-23 00:44:30 +1000

    Carousel next and back logic ready

--- client/src/work/index.js
+++ client/src/work/index.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
 import Radium from 'radium';
 import { connect } from 'react-redux';
-import { fadeIn } from 'react-animations';
-
 
 import { fetchWorks } from '../firebase/actions';
 
 class Work extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    }
+  }
+
   componentDidMount() {
     this.props.fetchWorks();
   }
 
-  navigate = () => {
+  navigateNext = () => {
+    const workListLength = this.props.worksList.length - 1;
+    const currentCount = this.state.count;
 
+    if (currentCount >= 0 && currentCount !== workListLength) {
+      this.setState({
+        count: currentCount + 1
+      });
+    } else {
+      this.setState({
+        count: 0
+      });
+    }
+  }
+
+  navigateBack = () => {
+    const workListLength = this.props.worksList.length - 1;
+    const currentCount = this.state.count;
+
+    if (currentCount >= 1) {
+      this.setState({
+        count: currentCount - 1
+      });
+    } else {
+      this.setState({
+        count: workListLength
+      });
+    }
   }
 
   renderWork() {
     if (this.props.worksList) {
-      return this.props.worksList.map((work) => {
-        return (
-          <div key={work.key} style={styles.worksImage}>{work.id}</div>
-        );
+      return this.props.worksList.map((work, index) => {
+        if (this.state.count === index) {
+          return (
+            <div key={work.key} style={styles.worksImage}>{work.id}</div>
+          );
+        }
       });
     }
   }
@@ -33,8 +67,8 @@ class Work extends Component {
         <div style={styles.workGallery}>
           {this.renderWork()}
         </div>
-        <div onClick={this.navigate} style={styles.navigation}>Next</div>
-        <div onClick={this.navigate} style={styles.navigation}>Back</div>
+        <div onClick={this.navigateNext} style={styles.navigation}>Next</div>
+        <div onClick={this.navigateBack} style={styles.navigation}>Back</div>
       </div>
     );
   }
